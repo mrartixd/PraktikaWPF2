e957fd107e5bb14c2ba782ce15166e116dbfa550
Artur <artur.shabunov@gmail.com>
2018-05-10 14:26:09 +0300

    add update, insert, delete group

--- Wpf2Praktika/Wpf2Praktika/Tables.xaml.cs
+++ Wpf2Praktika/Wpf2Praktika/Tables.xaml.cs
@@ -19,7 +19,8 @@ namespace Wpf2Praktika
     /// </summary>
     public partial class Tables : Window
     {
-        db_Artur_ShabunovEntities db;
+        //db_Artur_ShabunovEntities db;
+        db_Artur_ShabunovEntitiesInput db;
         public Tables()
         {
             InitializeComponent();
@@ -27,7 +28,35 @@ namespace Wpf2Praktika
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            db = new db_Artur_ShabunovEntities();
+            //db = new db_Artur_ShabunovEntities();
+            db = new db_Artur_ShabunovEntitiesInput();
+            grouptable.ItemsSource = db.tGroups.ToList();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            tGroup group = new tGroup();
+            group.NameGroup = groupname.Text;
+            db.tGroups.Add(group);
+            db.SaveChanges();
+            grouptable.ItemsSource = db.tGroups.ToList();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            int num = Convert.ToInt32(IDbox.Text);
+            var dRow = db.tGroups.Where(w => w.ID == num).FirstOrDefault();
+            db.tGroups.Remove(dRow);
+            db.SaveChanges();
+            grouptable.ItemsSource = db.tGroups.ToList();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            int num = Convert.ToInt32(IDbox.Text);
+            var uRow = db.tGroups.Where(w => w.ID == num).FirstOrDefault();
+            uRow.NameGroup = groupname.Text;
+            db.SaveChanges();
             grouptable.ItemsSource = db.tGroups.ToList();
         }
     }
